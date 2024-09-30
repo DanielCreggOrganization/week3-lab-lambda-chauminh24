8084e74166af01ce3646b628a59e22865707b28a
chauminh24 <114486211+chauminh24@users.noreply.github.com>
2024-09-30 13:35:36 +0000

    yeah

--- src/ie/atu/lambda/Main.java
+++ src/ie/atu/lambda/Main.java
@@ -2,7 +2,10 @@ package ie.atu.lambda;
 
 public class Main {
     public static void main(String[] args) {
-        MathOperation addition = new Addition();
-        System.out.println("Addition: " + addition.operate(int a, int b));
+        // MathOperation addition = new Addition();
+        // System.out.println("Addition: " + addition.operate(int a, int b));
+
+        MathOperation addition = (a, b) -> a+ b;
+        System.out.println(("Addition: " + addition.operate(5, 3)));
     }
 }
